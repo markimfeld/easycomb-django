5f2d3cec04552fb47bd521b1510a0c3729204d37
Marcos Imfeld <sebastianimfeld@gmail.com>
2020-06-09 18:43:11 -0300

    view orders

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from .models import Order
 
 
 def get_all_orders(request):
-    pass
+    return render(request, 'orders/orders.html', {
+        'orders': Order.objects.filter(status='P').all()
+    })
