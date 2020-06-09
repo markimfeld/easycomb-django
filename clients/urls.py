00bc076570d4e9ae8a8eb2498d565538d2228566
Marcos Imfeld <sebastianimfeld@gmail.com>
2020-06-09 13:05:25 -0300

    form to add clients

--- clients/urls.py
+++ clients/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'clients'
 urlpatterns = [
     path('', views.get_all_clients, name='customers'),
-    path('edit-customer/<int:pk>', views.edit_customer, name='edit-customer'),
+    path('new-customer', views.add_new_customer, name='new_customer'),
+    path('edit-customer/<int:pk>', views.edit_customer, name='edit_customer'),
     path('delete/customer/<int:pk>', views.delete_customer, name='delete_customer')
 ]
--- clients/views.py
+++ clients/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Customer
+from .forms import NewCustomerForm
 
 def get_all_clients(request):
     return render(request, 'clients/customers.html', {
         'customers': Customer.objects.all()
+    })
+
+def add_new_customer(request):
+    if request.method == 'POST':
+        form = NewCustomerForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+
+    return render(request, 'clients/customer-add.html', {
+        'form': NewCustomerForm()
     })
 
 def edit_customer(request, pk):
