d6d00af3addea25346c0faed9f6361f8951bbaf5
Marcos Imfeld <sebastianimfeld@gmail.com>
2020-06-09 19:35:11 -0300

    add list orders to the dashboard

--- easycomb_theme/views.py
+++ easycomb_theme/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from clients.models import Customer
 from inventory.models import Product, Combo
-
+from orders.models import Order
 
 def index(request):
 
     return render(request, 'easycomb_theme/index.html', {
+        'orders': Order.objects.all(),
+        'quantity_orders': Order.objects.filter(status='P').all().count,
         'quantity_products': Product.objects.all().count,
         'quantity_combos': Combo.objects.all().count,
-        'quantity_customers': Customer.objects.all().count
+        'quantity_customers': Customer.objects.filter(status=True).all().count
     })
