3dab24f051bcf9e9842a41f50df228f1fc155015
Marcos Imfeld <sebastianimfeld@gmail.com>
2020-06-13 10:58:03 -0300

    add total amount's combos pending to paid on customer's details view

--- clients/views.py
+++ clients/views.py
@@ -1,9 +1,32 @@
+from django.db.models import (
+    F,
+    ExpressionWrapper,
+    FloatField,
+    Sum
+)
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse
 
 # Create your views here.
 from .models import Customer
 from .forms import NewCustomerForm
+
+def calculate_total_orders(orders):
+    total = 0
+    for order in orders:
+        order_details = order.get_products.all().annotate(
+            subtotal_combos = ExpressionWrapper(
+                F('price_combo') * F('quantity'), output_field=FloatField()
+            ),
+            subtotal_products = ExpressionWrapper(
+                F('price_product') * F('quantity'), output_field=FloatField()
+            )
+        )
+        total += order_details.aggregate(Sum('subtotal_combos'))['subtotal_combos__sum']
+        total += order_details.aggregate(Sum('subtotal_products'))['subtotal_products__sum']
+    
+
+    return total
 
 def get_all_clients(request):
     return render(request, 'clients/customers.html', {
@@ -12,9 +35,14 @@ def get_all_clients(request):
 
 def get_customer_details(request, pk):
     customer = Customer.objects.get(pk=pk)
+
+    total_to_paid = calculate_total_orders(customer.orders.filter(paid_status=False).all())
+    print(total_to_paid)
+
     return render(request, 'clients/customer-details.html', {
         'customer': customer,
-        'orders': customer.orders.all()
+        'orders': customer.orders.all(),
+        'total_to_paid': total_to_paid
     })
 
 def add_new_customer(request):
