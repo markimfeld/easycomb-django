2701a2424b2d0d529683a10d032263778ad57819
Marcos Imfeld <sebastianimfeld@gmail.com>
2020-07-01 01:15:21 -0300

    add stats for combos and stock

--- easycomb_theme/urls.py
+++ easycomb_theme/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'easycomb_theme'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('api/data/', views.get_total_combos, name="api_data"),
 ]
--- easycomb_theme/views.py
+++ easycomb_theme/views.py
@@ -1,24 +1,43 @@
-from django.db.models import Count, Sum, Value as V
-from django.db.models.functions import Coalesce
+from django.db.models import Count, Sum, Value as V, F, ExpressionWrapper, FloatField, IntegerField
+from django.db.models.functions import Coalesce, Cast
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 from clients.models import Customer
 from inventory.models import Product, Combo
 from orders.models import Order, Status, OrderDetail
+from suppliers.models import PurchaseDetail
 
 def index(request):
 
     orders = Order.objects.all()
     order_details = OrderDetail.objects.all().select_related('combo', 'order')
+    
+    # startDate = request.GET.get('startDate')
+    # endDate = request.GET.get('endDate')
+
     # get total combos sales, only if had sales
-    most_selled_combos = order_details.exclude(combo=None).values('combo__name', 'combo__price').annotate(total_sales=Coalesce(Sum('quantity'), V(0))).order_by('-total_sales')
+    # most_selled_combos = order_details.filter(order__date__gte=startDate, order__date__lte=endDate) \
+     #   .exclude(combo=None).values('combo__name', 'combo__price').annotate(total_sales=Coalesce(Sum('quantity'), V(0))).order_by('-total_sales')
+
+
+    most_selled_combos = order_details.exclude(combo=None).values('combo__name', 'combo__price') \
+        .annotate(total_sales=Coalesce(Sum('quantity'), V(0))).order_by('-total_sales')
 
     status = Status.objects.first()
+    
+    
+    purchase_details = PurchaseDetail.objects.all().select_related('purchase'); 
+    stock_stats = purchase_details.filter(purchase__date__exact='2020-6-17') \
+        .annotate(total_percentages=Cast(((Cast(F('product__stock'), FloatField()) / Cast(F('quantity'), FloatField())) * 100.0), IntegerField())) \
+        .values('product__name', 'product__stock', 'quantity', 'total_percentages') 
+
 
     return render(request, 'easycomb_theme/index.html', {
         'most_selled_combos': most_selled_combos,
         'products': Product.objects.all(),
+        'stock_stats': stock_stats,
         'orders': Order.objects.all(),
         'quantity_orders': Order.objects.filter(status=status).all().count,
         'quantity_products': Product.objects.all().count,
@@ -26,4 +45,34 @@ def index(request):
         'quantity_customers': Customer.objects.filter(status=True).all().count
     })
 
+def get_total_combos(request, *args, **kwargs):
 
+    #startDate = request.GET.get('startDate')
+    #endDate = request.GET.get('endDate')
+
+    order_details = OrderDetail.objects.all().select_related('combo', 'order')
+    # get total combos sales, only if had sales
+    #most_selled_combos = order_details.filter(order__date__gte=startDate, order__date__lte=endDate) \
+    #    .exclude(combo=None).values('combo__name', 'combo__price').annotate(total_sales=Coalesce(Sum('quantity'), V(0))).order_by('-total_sales')
+
+
+    most_selled_combos = order_details.exclude(combo=None).values('combo__name', 'combo__price').annotate(total_sales=Coalesce(Sum('quantity'), V(0))).order_by('-total_sales')
+
+
+
+    status = Status.objects.first()
+
+    labels = [item['combo__name'] for item in most_selled_combos]
+    data = [item['total_sales'] for item in most_selled_combos]
+    backgroundColor = ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de', '#1A7BFF', '#FCC108', '#5A6268', '#3773A6'] 
+    donutData = {
+        'labels': labels,
+        'datasets': [
+            {
+                'data': data
+            },
+        ],
+        'backgroundColor': backgroundColor
+    }
+
+    return JsonResponse(donutData)
